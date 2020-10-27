6ef09c80d47b34ef6931d301b3d2c00f89be2fe7
Patt <peve.diaz@gmail.com>
2020-10-27 00:41:55 -0300

    login y formBuilder

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,6 +15,21 @@ export class HomeComponent implements OnInit {
   mensaje = "";
   secretNum = 5;
 
+  productos = [
+    {
+      id: 1,
+      descripcion: "Producto 1",
+      precio: 1000,
+    },
+    {
+      id: 2,
+      descripcion: "Producto 2",
+      precio: 1500,
+    },
+  ];
+
+  class_mensaje="";
+
   constructor() { }
 
   cambiarTitle() {
@@ -22,14 +37,35 @@ export class HomeComponent implements OnInit {
     this.modificado = true;
   }
   adivinar() {
+    this.productos = [
+      {
+        id: 3,
+        descripcion:"Producto 3",
+        precio: 2000,
+      },
+      {
+        id: 4,
+        descripcion: "Producto 4",
+        precio: 2500,
+      },
+      {
+        id: 5,
+        descripcion: "Producto 5",
+        precio: 3000,
+      }
+    ];
+
     this.intentos--;
     if (this.intentos > 0) {
       if (this.numero > this.secretNum) {
         this.mensaje = "Elegí un número más bajo";
+        this.class_mensaje = "perdio";
       } else if (this.numero < this.secretNum) {
         this.mensaje = "Elegí un número más alto";
+        this.class_mensaje = "perdio";
       } else {
         this.mensaje = "Ganaste!";
+        this.class_mensaje = "gano";
         this.desactivar = true;
       }
     } else {
